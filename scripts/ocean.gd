404c2298b598f7883f35b09eb924d5e0c26aff09
SPHI <115626927+SPHEI@users.noreply.github.com>
2025-04-12 15:40:24 +0200

    Better ocean
    idk if i'll do foam

--- scripts/ocean.gd
+++ scripts/ocean.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 func _ready():
-	var size = 255
+	var size = 256
 	
 	var vertices = PackedVector3Array()
 	var indices = PackedInt32Array()
@@ -8,18 +8,17 @@ func _ready():
 	for n in range(size*2):
 			for m in range(size):
 				vertices.push_back(Vector3(n, 0, m))
-				
 	for n in range(size*2-1):
 		for m in range(size-1):
 			var tl = n * size + m         
-			var tr = tl + 1                    
+			var tre = tl + 1                    
 			var bl = (n + 1) * size + m    
 			var br = bl + 1                  
 			indices.push_back(tl)
 			indices.push_back(bl)
-			indices.push_back(tr)
+			indices.push_back(tre)
 			
-			indices.push_back(tr)
+			indices.push_back(tre)
 			indices.push_back(bl)
 			indices.push_back(br)
 			
@@ -33,4 +32,4 @@ func _ready():
 	mesh = arr_mesh
 	
 	mesh.set_custom_aabb(mesh.get_aabb().grow(1000.0))
-	position += Vector3(-255,0,-128)
+	position += Vector3(255,0,128)
